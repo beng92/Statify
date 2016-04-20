6ec25eb2c4c2c1f79bff597c655991117d41270c
Ben Goodwin <bengoodwin13@gmail.com>
2016-04-20 22:16:38 +0100

    Finished caching class

--- src/StatifyCache.py
+++ src/StatifyCache.py
@@ -1,20 +1,25 @@
 import spotipy
+import xml.etree.ElementTree as ET
+
 
 class StatifyCache:
     def __init__(self):
         self.spotipy = spotipy.Spotify()
-        file = open("data/cache.xml",'r')
-        read = file.read()
-    
+        self.tree = ET.parse("data/cache.xml")
+        self.root = self.tree.getroot()
+        self.allSongs   = self.root[0]
+        self.allArtists = self.root[1]
+        self.allAlbums  = self.root[2]
+        
     def add(self, songid, artistid, albumid):
-        if(songid != None):
-            self.addSong(songid)
-        if(artistid != None):
-            self.addArtist(artistid)
-        if(albumid != None):
-            self.addAlbum(albumid)
+        if(songid != None and not self.exists(id, "song")):
+            self._addSong(songid)
+        if(artistid != None and not self.exists(id, "artist")):
+            self._addArtist(artistid)
+        if(albumid != None and not self.exists(id, "album")):
+            self._addAlbum(albumid)
     
-    def addSong(self, id):
+    def _addSong(self, id):
         result = self.spotipy.track(id)
         name     = result['name']
         songurl  = result['external_urls']['spotify']
@@ -26,16 +31,16 @@ class StatifyCache:
         discno   = result['disc_number']
         trackno  = result['track_number']
         duration = result['duration_ms']
-        song = Song(id, name, songurl, artist, artistid, album, albumid, explicit, discno, trackno, duration)
+        return Song(self.tree, id, name, songurl, artist, artistid, album, albumid, explicit, discno, trackno, duration)
     
-    def addArtist(self, id):
+    def _addArtist(self, id):
         result = self.spotipy.artist(id)
         name      = result['name']
         artisturl = result['external_urls']['spotify']
         imageurl  = result['images'][0]['url']
-        artist = Artist(id, name, artisturl, imageurl)
+        return Artist(self.tree, id, name, artisturl, imageurl)
         
-    def addAlbum(self, id):
+    def _addAlbum(self, id):
         result = self.spotipy.album(id)
         name        = result['name']
         artist      = result['artists'][0]['name']
@@ -48,35 +53,168 @@ class StatifyCache:
             tracks.append(track['name'])
             duration += track['duration_ms']
         totaltracks = result['tracks']['total']
-        album = Album(id, name, artist, artistid, albumurl, imageurl, tracks, totaltracks, duration)
-        
-        
+        return Album(self.tree, id, name, artist, artistid, albumurl, imageurl, tracks, totaltracks, duration)
+    
+    def exists(self, id, type):
+        found = False
+        if type == "song" or type == "any":
+            for track in self.allSongs.findall('song'):
+                if track.find('id').text == id:
+                    found = True 
+        if type == "artist"or type == "any":
+            for artist in self.allArtists.findall('artist'):
+                if artist.find('id').text == id:
+                    found = True
+        if type == "album" or type == "any":
+            for album in self.allAlbums.findall('album'):
+                if album.find('id').text == id:
+                    found = True
+        else:
+            return None
+        return found
+    
+    def get(self, id):
+        if self.exists(id, "any"):
+            for track in self.allSongs.findall('song'):
+                if track.find('id').text == id:
+                    return track
+            for artist in self.allArtists.findall('artist'):
+                if artist.find('id').text == id:
+                    return artist
+            for album in self.allAlbums.findall('album'):
+                if album.find('id').text == id:
+                    return album
+        else:
+            return None
+            
 class Song: 
-    def __init__(self, id, name, songurl, artist, artistid, album, albumid, explicit, discno, trackno, duration):
-        print ("Song:", id, name, songurl, artist, artistid, album, albumid, explicit, discno, trackno, duration)
+    def __init__(self, tree, id, name, songurl, artist, artistid, album, albumid, explicit, discno, trackno, duration):
+        self.id = id
+        self.name = name
+        self.songurl = songurl
+        self.artist = artist
+        self.artistid = artistid
+        self.album = album
+        self.albumid = albumid
+        self.explicit = explicit
+        self.discno = discno
+        self.trackno = trackno
+        self.duration = duration
+        
+        self.tree = tree
+        self.write()
+        #print ("Song:", id, name, songurl, artist, artistid, album, albumid, explicit, discno, trackno, duration)
     
     def write(self):
-        pass
+        new = ET.Element('song')
+        
+        newid           = ET.SubElement(new, 'id')
+        newname         = ET.SubElement(new, 'name')
+        newsongurl      = ET.SubElement(new, 'songurl')
+        newartist       = ET.SubElement(new, 'artist')
+        newartistid     = ET.SubElement(new, 'artistid')
+        newalbum        = ET.SubElement(new, 'album')
+        newalbumid      = ET.SubElement(new, 'albumid')
+        newexplicit     = ET.SubElement(new, 'explicit')
+        newdiscno       = ET.SubElement(new, 'discno')
+        newtrackno      = ET.SubElement(new, 'trackno')
+        newduration     = ET.SubElement(new, 'duration')
+       
+        
+        newid.text          = self.id 
+        newname.text        = self.name 
+        newsongurl.text     = self.songurl 
+        newartist.text      = self.artist 
+        newartistid.text    = self.artistid 
+        newalbum.text       = self.album
+        newalbumid.text     = self.albumid
+        newexplicit.text    = str(self.explicit)
+        newdiscno.text      = str(self.discno)
+        newtrackno.text     = str(self.trackno)
+        newduration.text    = str(self.duration)
+        
+        self.tree.getroot()[0].append(new)
+        self.tree.write("data/cache.xml")
         
         
 class Artist: 
-    def __init__(self, id, name, artisturl, imageurl):
-        print ("Artist:", id, name, artisturl, imageurl)
+    def __init__(self, tree, id, name, artisturl, imageurl):
+        self.id = id
+        self.name = name
+        self.artisturl = artisturl
+        self.imageurl = imageurl
+        
+        self.tree = tree
+        self.write()
+        #print ("Artist:", id, name, artisturl, imageurl)
     
     def write(self):
-        pass
+        new = ET.Element('artist')
+        
+        newid        = ET.SubElement(new, 'id')
+        newname      = ET.SubElement(new, 'name')
+        newartisturl = ET.SubElement(new, 'artisturl')
+        newimageurl  = ET.SubElement(new, 'imageurl')
+        
+        newid.text        = self.id
+        newname.text      = self.name
+        newartisturl.text = self.artisturl
+        newimageurl.text  = self.imageurl
+        
+        self.tree.getroot()[1].append(new)
+        self.tree.write("data/cache.xml")
         
         
 class Album: 
-    def __init__(self, id, name, artist, artistid, albumurl, imageurl, tracks, totaltracks, duration):
-        print ("Album:", id, name, artist, artistid, albumurl, imageurl, tracks, totaltracks, duration)
+    def __init__(self, tree, id, name, artist, artistid, albumurl, imageurl, tracks, totaltracks, duration):
+        self.id = id
+        self.name = name
+        self.artist = artist
+        self.artistid = artistid
+        self.albumurl = albumurl
+        self.imageurl = imageurl
+        self.tracks = tracks
+        self.totaltracks = totaltracks
+        self.duration = duration
+        
+        self.tree = tree
+        self.write()
+        #print ("Album:", id, name, artist, artistid, albumurl, imageurl, tracks, totaltracks, duration)
     
     def write(self):
-        pass
+        new = ET.Element('album')
+        
+        newid           = ET.SubElement(new, 'id')
+        newname         = ET.SubElement(new, 'name')
+        newartist       = ET.SubElement(new, 'artist')
+        newartistid     = ET.SubElement(new, 'artistid')
+        newalbumurl     = ET.SubElement(new, 'albumurl')
+        newimageurl     = ET.SubElement(new, 'imageurl')
+        newtracks       = ET.SubElement(new, 'tracks')
+        newtotaltracks  = ET.SubElement(new, 'totaltracks')
+        newduration     = ET.SubElement(new, 'duration')
+       
+        
+        newid.text          = self.id 
+        newname.text        = self.name 
+        newartist.text      = self.artist 
+        newartistid.text    = self.artistid 
+        newalbumurl.text    = self.albumurl
+        newimageurl.text    = self.imageurl
+        newtracks.text      = str(self.tracks)
+        newtotaltracks.text = str(self.totaltracks)
+        newduration.text    = str(self.duration)
+        
+        self.tree.getroot()[2].append(new)
+        self.tree.write("data/cache.xml")
         
         
 def test():
     sc = StatifyCache()
-    sc.add("2ELcuwXrtMA8ect9cGTYnQ","2Dr744zaEbNqmW9jxw4gfq","2PqYzY7wdgq0ydlC3nR4ei")
+    #print(sc.exists("2ELcuwXrtMA8ect9cGTYnQ", "song")
+    #sc.add("2ELcuwXrtMA8ect9cGTYnQ","2Dr744zaEbNqmW9jxw4gfq","2PqYzY7wdgq0ydlC3nR4ei")
+    print(sc.exists("2Dr744zaEbNqmW9jxw4gfq", "any"))
+    print(sc.exists("2Dr744zaEbNqsjxw4gfq", "any"))
+    print(sc.get("2PqYzY7wdgq0ydlC3nR4ei"))
 
 test()
